ef0985d427f65eff49c9748871370b3ccdfc96b5
KimSoobin32 <sbkim@exwaysoft.com>
2021-04-14 18:03:42 +0900

    1. create, read 완

---
+++ vue-board/src/data/index.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        writer: 'a',
+        title: 'abc',
+        content: '1'
+    },
+    {
+        writer: 'b',
+        title: 'abc2',
+        content: '2'
+    },
+    {
+        writer: 'c',
+        title: 'abc3',
+        content: '3'
+    }
+]
