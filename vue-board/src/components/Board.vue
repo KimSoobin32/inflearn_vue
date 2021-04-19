42843461514c7b54090024003bdc118f6bab4594
KimSoobin32 <sbkim@exwaysoft.com>
2021-04-19 11:26:23 +0900

    5. 페이지네이션 완

--- vue-board/src/components/Board.vue
+++ vue-board/src/components/Board.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <b-table striped hover :items="items" :fields="fields" @row-clicked="rowClick"></b-table>
+    <b-table striped hover :items="items" :per-page="perPage" :current-page="currentPage" :fields="fields" @row-clicked="rowClick"></b-table>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      align="center"
+    ></b-pagination>
     <b-button @click="writeContent">글쓰기</b-button>
   </div>
 </template>
@@ -15,6 +21,8 @@ import data from '@/data';
         let items = data.Content.sort((a,b) => {return b.content_id - a.content_id})
         items = items.map(contentItem => {return {...contentItem, user_name: data.User.filter(userItem => userItem.user_id === contentItem.user_id)[0].name}})
       return {
+        currentPage: 1,
+        perPage: 10,
        fields: [
         {
           key: 'content_id',
@@ -49,6 +57,11 @@ import data from '@/data';
                 path:'/board/free/create2'
             })
         }
+    },
+    computed: {
+      rows() {
+        return this.items.length
+      }
     }
   }
 </script>
